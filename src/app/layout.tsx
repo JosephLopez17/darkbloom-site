56176fee0dfb89f2d3c14d09ba69c4223197878c
JosephLopez17 <131908825+JosephLopez17@users.noreply.github.com>
2025-10-19 09:16:26 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
